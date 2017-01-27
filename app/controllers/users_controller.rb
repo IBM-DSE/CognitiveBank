ccc2767ca544773e63581b13ee62d67305805d75
David Thomason <dtom90@users.noreply.github.com>
2017-01-27 13:46:39 -0500

    Added transactions with transactions table and category pie chart in user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def profile
+    if logged_in?
+      cc = {}
+      for category in TransactionCategory.all
+        cc[category.category] = current_user.transactions.where(category: category.category).count
+      end
+      @sorted_categories = cc.sort_by {|k,v| v}.reverse.to_h
+    else
+      redirect_to login_path, flash: {danger: 'You must log in to view your profile'}
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
-  
   validates :name, presence: true, length: { maximum: 50 }
   
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, 
-            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 },
+            format:           { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
+  
+  has_many :transactions
+  
+  def self.category_counts
+    
+  end
+  
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :

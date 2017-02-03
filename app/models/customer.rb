7983757b35e6d0df6583a27d61ad2622a8f027d2
David Thomason <dtom90@users.noreply.github.com>
2017-02-03 11:53:40 -0500

    Churn only requested in production (Bluemix)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,6 +9,9 @@ class Customer < ApplicationRecord
   SCORING_ML_URL      = 'https://prod-scoring-ml.spark.bluemix.net:32768/v1/score/581'
   SCORING_ML_RESOURCE = RestClient::Resource.new SCORING_ML_URL
   
+  def get_churn
+    puts post_to_scoring_ml ['F', 38, 2, 114368, 3852862, 5, 700259, 0.0, 335, 2090, 3]
+  end
   #where:
   #SEX: F
   #AGE: 38
@@ -21,13 +24,20 @@ class Customer < ApplicationRecord
   #YRLY_TX: 335
   #AVG_TX_AMT: 2090
   #NEGTWEETS: 3
-  def get_churn
-    puts post_to_scoring_ml ['F', 38, 2, 114368, 3852862, 5, 700259, 0.0, 335, 2090, 3]
-  end
   
   def post_to_scoring_ml(record)
-    SCORING_ML_RESOURCE.post({ 'record': record }.to_json,
-                             { :content_type => 'application/json', :Authorization => '5tJjGrhtD1KRyGgv0oPufA==' })
+    
+    headers = { :content_type => 'application/json', :Authorization => '5tJjGrhtD1KRyGgv0oPufA==' }
+    body = { 'record': record }.to_json
+    begin
+      SCORING_ML_RESOURCE.post(body, headers)
+    rescue Exception => ex
+      puts "ERROR: #{ex.response}"
+      puts "Scoring Endpoint = #{SCORING_ML_RESOURCE}"
+      puts "Body sent to Scoring: #{body}"
+      raise ex
+    end
+    
   end
   
   def gender

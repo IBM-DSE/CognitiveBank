ba6ea6e3cece784559a88e59c07886d82761857f
David Thomason <dlthomas@us.ibm.com>
2017-02-09 19:09:51 -0500

    Final offer displayed automatically only if churn prediction is positive Server logging displays activity during chatbot conversation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,18 +11,74 @@ class Customer < ApplicationRecord
   SCORING_ML_URL      = 'https://prod-scoring-ml.spark.bluemix.net:32768/v1/score/581'
   SCORING_ML_RESOURCE = RestClient::Resource.new SCORING_ML_URL
   
-  #where:
-  #SEX: F
-  #AGE: 38
-  #EDUCATION: 2
-  #INVESTMENT: 114368
-  #INCOME: 3852862
-  #ACTIVITY: 5
-  #YRLY_AMT: 700259
-  #AVG_DAILY_TX: 0.0
-  #YRLY_TX: 335
-  #AVG_TX_AMT: 2090
-  #NEGTWEETS: 3
+  def get_churn(debug=false)
+    if debug
+      puts ' '
+      puts 'Fetching churn probability from MLz churn model.'
+      puts 'Sending cutsomer attributes to churn scoring: '
+      puts churn_attrs.to_s
+    end
+    if Rails.env.production?
+      response = post_to_scoring_ml churn_attrs.values
+      process_json(response)
+    else
+      load_sample_ml_result
+    end
+  end
+  
+  def start_conversation
+    # Send empty string to Watson Conversation
+    if messages.empty?
+      puts ' '
+      puts 'Starting conversation with the customer...'
+      Message.send_to_watson_conversation('', self)
+    end
+  end
+  
+  def name
+    self.user.name
+  end
+  
+  def twitter_id
+    self.twitter_personality.username
+  end
+  
+  def gender
+    self.gender ? 'Male' : 'Female'
+  end
+  
+  def self.print_churn(churn)
+    puts ' '
+    puts 'Customer churn result: '
+    churn.each do |k,v|
+      puts "  #{k.to_s.humanize}: #{v}"
+    end
+  end
+  
+  private
+
+  def post_to_scoring_ml(record)
+  
+    headers = { :content_type => 'application/json', :Authorization => '5tJjGrhtD1KRyGgv0oPufA==' }
+    body    = { 'record': record }.to_json
+    begin
+      SCORING_ML_RESOURCE.post(body, headers)
+    rescue Exception => ex
+      puts "ERROR: #{ex.response}"
+      puts "Scoring Endpoint = #{SCORING_ML_RESOURCE}"
+      puts "Body sent to Scoring: #{body}"
+      raise ex
+    end
+  end
+  
+  def process_json(json)
+    body        = JSON.parse(json)
+    result = body['result']
+    churn = result['prediction'] == 1.0
+    prob = result['probability']['values'][result['prediction'].to_i]
+    {churn: churn, probability: (prob*100.0).round(2).to_s+'%'}
+  end
+
   def churn_attrs
     self.attributes.slice(
         'sex',
@@ -38,54 +94,26 @@ class Customer < ApplicationRecord
         'negtweets'
     )
   end
-  
-  def get_churn
-    response = post_to_scoring_ml self.churn_attrs.values
-    extract_from_json(response)
-  end
-  
+
   def load_sample_ml_result
     file = File.read('db/churn_result.json')
-    extract_from_json(file)
+    process_json(file)
   end
   
-  
-  def start_conversation
-    # Send empty string to Watson Conversation
-    Message.send_to_watson_conversation('', self) if messages.empty?
-  end
-  
-  def post_to_scoring_ml(record)
-    
-    headers = { :content_type => 'application/json', :Authorization => '5tJjGrhtD1KRyGgv0oPufA==' }
-    body    = { 'record': record }.to_json
-    begin
-      SCORING_ML_RESOURCE.post(body, headers)
-    rescue Exception => ex
-      puts "ERROR: #{ex.response}"
-      puts "Scoring Endpoint = #{SCORING_ML_RESOURCE}"
-      puts "Body sent to Scoring: #{body}"
-      raise ex
-    end
-  
-  end
-  
-  def twitter_id
-    self.twitter_personality.username
-  end
-  
-  def gender
-    self.gender ? 'Male' : 'Female'
-  end
-  
-  private
-  
-  def extract_from_json(json)
-    body        = JSON.parse(json)
-    result = body['result']
-    churn = result['prediction'] == 1.0
-    prob = result['probability']['values'][result['prediction'].to_i]
-    {churn: churn, probability: (prob*100.0).round(2).to_s+'%'}
+  def sample_input
+    {
+        sex: 'F',
+        age: 38,
+        education: 2,
+        investment: 114368,
+        income: 3852862,
+        activity: 5,
+        yrly_amt: 700259,
+        avg_daily_tx: 0.0,
+        yrly_tx: 335,
+        avg_tx_amt: 2090,
+        negtweets: 3
+    }
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -22,6 +22,8 @@ class Message < ApplicationRecord
 
   # send self to watson conversation
   def send_to_watson
+    puts ' '
+    puts "#{customer.name} says: \"#{content}\""
     Message.send_to_watson_conversation content, customer
   end
   
@@ -45,16 +47,38 @@ class Message < ApplicationRecord
   def self.update_customer(results, customer)
     # update user context
     customer.context = results[:context]
-    puts customer.context
-    
-    # create watson messages
+
+    # print intents and entities
+    if results[:intents][0] and results[:intents][0][:confidence] > 0.9 or results[:entities][0]
+      puts ' '
+      puts 'Watson NLP results:'
+    end
+    if results[:intents][0] and results[:intents][0][:confidence] > 0.9
+      puts "Intent: #{results[:intents][0][:intent].upcase}"
+    end
+    if results[:entities][0]
+      range = results[:entities][0][:location]
+      puts "Entities: #{results[:input][:text][range[0]..range[1]].upcase}"
+    end
+
+    # store watson messages
     results[:output][:text].each do |line|
       msg = customer.messages.build content: line, watson_response: true
-      puts msg.content
+      puts ' '
+      puts "Watson response: \"#{msg.content}\""
     end
-    
+
     # save the customer data
     customer.save
+    
+    if results[:output][:action]
+      puts ' '
+      puts 'Watson Conversation Action Signal: '+results[:output][:action]
+      churn_res = customer.get_churn true
+      Customer.print_churn(churn_res)
+      send_to_watson_conversation('churn', customer) if churn_res[:churn]
+    end
+    
   rescue Exception => e
     puts e
   end

f8de5d66f9aa350cee12310d7ec548a3274fbb15
David Thomason <dtom90@users.noreply.github.com>
2017-02-13 09:01:54 -0500

    ML_Scoring updates shorter open_timeout call to scoring from dashboard open

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,10 +7,16 @@ class Customer < ApplicationRecord
   has_many :messages
   
   serialize :context, JSON
+
+  attr_reader :churn_result
   
   def start_conversation
     # Send empty string to Watson Conversation
     Message.send_to_watson_conversation('', self) if messages.empty?
+  end
+  
+  def get_churn
+    @churn_result = ML_Scoring.new self
   end
   
   def name

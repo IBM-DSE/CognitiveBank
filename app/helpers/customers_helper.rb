5086ecfe3b6ac42d080e6f6683e9589e28e6d21f
David Thomason <dlthomas@us.ibm.com>
2017-02-15 09:32:03 -0500

    More reasonable amount ranges

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,6 +1,6 @@
 module CustomersHelper
   
-  MAX=1000
+  MAX=100
   
   MIN_AIRFARE=1000
   MAX_AIRFARE=5000
@@ -8,8 +8,8 @@ module CustomersHelper
   MIN_HOTEL=300
   MAX_HOTEL=1000
   
-  MIN_DINING=100
-  MAX_DINING=250
+  MIN_DINING=80
+  MAX_DINING=150
 
   MIN_SAFARI=800
   MAX_SAFARI=1200

fc1efedd3b7f8a3340d6d396c17a17e8a22f4c90
David Thomason <dlthomas@us.ibm.com>
2017-03-15 15:21:25 -0400

    Switched MlScoring to use MlScoringService

--- app/lib/ml_scoring.rb
+++ app/lib/ml_scoring.rb
@@ -3,13 +3,16 @@ class MlScoring
   attr_reader :result
   
   def initialize(customer)
-    @customer_attrs = customer.attributes.slice(*SCORING_ATTRS).values
+    customer_attrs = customer.attributes.slice(*SCORING_ATTRS).values
     puts ' '
     puts 'Fetching churn probability from MLz churn model.'
     puts 'Sending customer attributes to churn scoring: '
-    puts @customer_attrs.to_s
+    puts customer_attrs.to_s
     
-    post_to_scoring_ml
+    # post_to_scoring_ml
+    score = MlScoringService.get_score(customer_attrs)
+
+    process_score score
     
     print_churn_result
   end
@@ -30,36 +33,10 @@ class MlScoring
   
   SCORING_ATTRS = %w(age activity education sex state negtweets income)
   
-  SCORING_CALL_TIMEOUT = (ENV['ML_SCORING_TIMEOUT'] || 2).to_i
-  ML_SCORING_RESOURCE = RestClient::Resource.new ENV['ML_SCORING_URL'], 
-                                                 :read_timeout => SCORING_CALL_TIMEOUT, 
-                                                 :open_timeout => SCORING_CALL_TIMEOUT
-  
-  def get_token
-    
-  end
-  
-  def post_to_scoring_ml
-    puts 'Posting to endpoint '+ML_SCORING_RESOURCE.url
-    headers = { :content_type => 'application/json', :Authorization => ENV['ML_SCORING_AUTH'] }
-    body    = { 'Record': @customer_attrs }.to_json
-    begin
-      puts headers
-      puts body
-      response = ML_SCORING_RESOURCE.post(body, headers)
-      puts 'Scoring request successful!'
-      process_json response
-    rescue Exception => e
-      puts 'ERROR: '+e.message
-      load_sample_ml_result
-    end
-  end
-  
-  def process_json(json)
-    result  = JSON.parse(json)
-    puts result
-    churn   = result['prediction'] == 1.0
-    prob    = result['probability']['values'][result['prediction'].to_i]
+  def process_score(score)
+    puts score
+    churn   = score['prediction'] == 1.0
+    prob    = score['probability']['values'][score['prediction']]
     @result = { prediction: churn, probability: (prob*100.0).round(2).to_s+'%' }
   end
   
@@ -71,10 +48,4 @@ class MlScoring
     end
   end
   
-  def load_sample_ml_result
-    puts 'loading sample'
-    file = File.read('db/churn_result.json')
-    process_json(file)
-  end
-
 end

a6f6e6c2b54e43b4d6e5e299ed83efc6086626a0
David Thomason <dtom90@users.noreply.github.com>
2017-03-15 15:21:25 -0400

    Switched MlScoring to use MlScoringService

--- app/models/ml_scoring_service.rb
+++ app/models/ml_scoring_service.rb
@@ -1,14 +1,19 @@
 class MlScoringService < ApplicationRecord
   
+  def self.get_score(customer_attrs)
+    self.first.get_score customer_attrs
+  end
+  
+  def get_score(customer_attrs)
+    token = get_token
+    post_to_scoring_ml token, customer_attrs
+  end
+  
   def get_token
     response = RestClient.post(ldap_url, creds.to_json, content_type: :json)
     JSON.parse(response)['token']
   end
   
-  def get_score
-    token = get_token
-    post_to_scoring_ml token
-  end
   
   def test_ldap
     token = get_token
@@ -16,7 +21,7 @@ class MlScoringService < ApplicationRecord
   end
   
   def test_score
-    score = get_score
+    score = get_score SAMPLE_RECORD
     score.is_a?(Hash) and
         score.keys.include? 'prediction' and
         [1, 0].include? score['prediction'] and
@@ -28,7 +33,7 @@ class MlScoringService < ApplicationRecord
   
   TOKEN_PREFIX  = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9'
   SCORING_ATTRS = %w(AGE ACTIVITY EDUCATION SEX STATE NEGTWEETS INCOME)
-  SAMPLE_RECORD = [41, 4, 3, 'M', 'TX', 12, 316530]
+  SAMPLE_RECORD = [26, 2, 3, 'M', 'NY', 0, 483620]
   
   def ldap_url
     "http://#{self.hostname}:#{self.ldap_port}/v2/identity/ldap"
@@ -42,10 +47,10 @@ class MlScoringService < ApplicationRecord
     "http://#{self.hostname}:#{self.scoring_port}/wml/scoring/spark/deployments/#{self.deployment}/predict"
   end
   
-  def post_to_scoring_ml(token)
+  def post_to_scoring_ml(token, customer_attrs)
     puts 'Posting to endpoint '+scoring_url
     headers = { :content_type => 'application/json', :Authorization => 'Bearer '+token }
-    body    = { 'Record': SAMPLE_RECORD }.to_json
+    body    = { 'Record': customer_attrs }.to_json
     puts headers
     puts body
     begin
@@ -57,5 +62,10 @@ class MlScoringService < ApplicationRecord
       false
     end
   end
+
+  # SCORING_CALL_TIMEOUT = (ENV['ML_SCORING_TIMEOUT'] || 2).to_i
+  # ML_SCORING_RESOURCE = RestClient::Resource.new ENV['ML_SCORING_URL'],
+  #                                                :read_timeout => SCORING_CALL_TIMEOUT,
+  #                                                :open_timeout => SCORING_CALL_TIMEOUT
 
 end

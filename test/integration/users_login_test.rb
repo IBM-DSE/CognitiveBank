392e559cef99cc4a278043fa89b5cc444e86ac23
David Thomason <dtom90@users.noreply.github.com>
2017-03-22 15:25:02 -0400

    Updated tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -27,6 +27,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', profile_path
     assert_select 'a[href=?]', logout_path
+    
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url

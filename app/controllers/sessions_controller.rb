96316bc72df64f8b9e1d3f6ce32b8f0d4228a25d
David Thomason <dlthomas@us.ibm.com>
2017-03-22 15:10:04 -0400

    Customer churn prediction and probability values reset on logout and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      current_customer.clear_conversation if is_customer?
+      current_customer.reset if is_customer?
       redirect_to user.admin ? admin_path : dashboard_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    current_customer.clear_conversation if is_customer?
+    current_customer.reset if is_customer?
     log_out
     redirect_to root_path
   end

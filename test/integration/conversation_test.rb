b49103f1981ce0c8acbd48f74103f1a48fb3c872
David Thomason <dtom90@users.noreply.github.com>
2017-03-23 17:51:06 -0400

    More graceful handling of Conversation error Added test for bad conversation worksapce

--- test/integration/conversation_test.rb
+++ test/integration/conversation_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ConversationTest < ActionDispatch::IntegrationTest
   
-  INTRO = "Hi there, I\\'m the Cognitive Bank Virtual Assistant! I can help you find a good offer. Interested?"
+  INTRO      = "Hi there, I\\'m the Cognitive Bank Virtual Assistant! I can help you find a good offer. Interested?"
+  BAD_CONFIG = "Oops! Looks like I haven\\'t been configured correctly to speak with Watson."
   
   def setup
     @user = users(:bruce)
@@ -10,22 +11,34 @@ class ConversationTest < ActionDispatch::IntegrationTest
   
   test 'good conversation' do
     log_in_as(@user, password: 'password')
-    post '/messages', params: { message: {content: '', context: ''} }, xhr: true, headers: { accept: 'text/javascript' }
+    post '/messages', params: { message: { content: '', context: '' } }, xhr: true, headers: { accept: 'text/javascript' }
     assert_response :success
     assert_select 'img'
     assert_includes @response.body, 'watson-message'
     assert_includes @response.body, INTRO.html_safe
   end
-
-  test 'bad conversation' do
+  
+  test 'bad conversation workspace' do
+    good_workspace      = ENV['WORKSPACE_ID']
+    ENV['WORKSPACE_ID'] = 'FOOBAR'
+    log_in_as(@user, password: 'password')
+    post '/messages', params: { message: { content: '', context: '' } }, xhr: true, headers: { accept: 'text/javascript' }
+    assert_response :success
+    assert_select 'img'
+    assert_includes @response.body, 'watson-message'
+    assert_includes @response.body, BAD_CONFIG.html_safe
+    ENV['WORKSPACE_ID'] = good_workspace
+  end
+  
+  test 'bad socring service conversation' do
     MlScoringService.set_main ml_scoring_services(:wsc_gateway_bad_hostname)
     log_in_as(@user, password: 'password')
-    post '/messages', params: { message: {content: '', context: ''} }, xhr: true, headers: { accept: 'text/javascript' }
+    post '/messages', params: { message: { content: '', context: '' } }, xhr: true, headers: { accept: 'text/javascript' }
     assert_response :success
     assert_select 'img'
     assert_includes @response.body, 'watson-message'
     assert_includes @response.body, INTRO.html_safe
     MlScoringService.set_main ml_scoring_services(:wsc_gateway)
   end
-  
+
 end

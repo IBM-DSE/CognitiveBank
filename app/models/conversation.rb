042af4f832819f0de45eed8975815a73d477c152
David Thomason <dlthomas@us.ibm.com>
2017-03-23 17:51:06 -0400

    More graceful handling of Conversation error Added test for bad conversation worksapce

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,38 @@
 class Conversation
   
+  
+  # class method for sending string message content and customer context to WC
+  def self.send(customer, message, context)
+    
+    # if this is the first message in the conversation, check to see if the customer will churn
+    context[:will_churn] = customer.will_churn? if context.empty?
+    
+    # construct payload from input message and context
+    body = { input: { text: message }, context: context }.to_json
+    
+    begin
+      
+      # send the payload to Conversation service
+      # response = CONVERSATION_RESOURCE.post(body, :content_type => 'application/json')
+      response = RestClient::Request.execute method:  :post, url: conversation_url, payload: body,
+                                             headers: { content_type: :json, accept: :json },
+                                             user:    USERNAME, password: PASSWORD
+      
+      # slice the output and context
+      JSON.parse(response.body).deep_symbolize_keys!.slice(:output, :context)
+    
+    rescue => e
+      STDERR.puts "ERROR: #{e}"
+      STDERR.puts "Conversation Endpoint = #{CONVERSATION_RESOURCE}"
+      STDERR.puts "Body sent to Watson Conversation: #{body}"
+      STDERR.puts e.backtrace.select {|l| l.start_with? Rails.root.to_s }
+      {output: { text: ["Oops! Looks like I haven't been configured correctly to speak with Watson."]}, context: {}}
+    end
+  end
+  
+  
+  private
+  
   API_ENDPOINT='https://gateway.watsonplatform.net/conversation/api/v1/workspaces/'
   VERSION     = '2016-09-20'
   
@@ -16,29 +49,8 @@ class Conversation
   URL                   = API_ENDPOINT + WORKSPACE_ID + '/message?version=' + VERSION
   CONVERSATION_RESOURCE = RestClient::Resource.new URL, USERNAME, PASSWORD
   
-  # class method for sending string message content and customer context to WC
-  def self.send(customer, message, context)
-    
-    # if this is the first message in the conversation, check to see if the customer will churn
-    context[:will_churn] = customer.will_churn? if context.empty?
-    
-    # construct payload from input message and context
-    body = { input: { text: message }, context: context }.to_json
-    
-    begin
-      
-      # send the payload to Conversation service
-      response = CONVERSATION_RESOURCE.post(body, :content_type => 'application/json')
-      
-      # slice the output and context
-      JSON.parse(response.body).deep_symbolize_keys!.slice(:output, :context)
-    
-    rescue => e
-      STDERR.puts "ERROR: #{e}"
-      STDERR.puts "Conversation Endpoint = #{CONVERSATION_RESOURCE}"
-      STDERR.puts "Body sent to Watson Conversation: #{body}"
-      # raise e
-    end
+  def self.conversation_url
+    API_ENDPOINT + ENV['WORKSPACE_ID'] + '/message?version=' + VERSION
   end
 
 end

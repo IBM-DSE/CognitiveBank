c67433135ec8ac6f4b9269ba6618ec067eace160
David Thomason <dlthomas@us.ibm.com>
2017-07-31 13:28:17 -0400

    Added Bruce feature spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'HomePage', type: :feature do
-  scenario 'User creates a new widget' do
+  scenario 'User visits home page' do
     visit '/'
     expect(page).to have_text 'Cognitive Bank'
   end

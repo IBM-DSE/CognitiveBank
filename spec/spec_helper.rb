57f65228a4d2e0715ef7ad9fe362f1c1085b461a
David Thomason <dlthomas@us.ibm.com>
2017-08-01 16:43:56 -0400

    Made RSpec use javascript and included first chatbot message added poltergeist and selenium-webdriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 12
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

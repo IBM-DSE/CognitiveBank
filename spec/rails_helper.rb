d6abb218b4af8db74a8881e790bebef72c2e1d55
David Thomason <dtom90@users.noreply.github.com>
2017-08-02 10:01:12 -0400

    Added DatabaseCleaner and seeding test db before each test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    Rails.application.load_seed # loading seeds
+  end
+
 end

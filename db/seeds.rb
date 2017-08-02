881c88ac96fd8c8c375f96dabd57eea01adfa201
David Thomason <dlthomas@us.ibm.com>
2017-08-02 10:01:37 -0400

    Cleaned up seeds.rb so that it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,70 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'csv'
 
-
-# Load the twitter personalities
-CSV.foreach('db/bruce_twitter.csv', headers: true) do |row|
-  tp = TwitterPersonality.new(row.to_hash.except('category'))
-  tp.save
-end
-puts "Loaded #{TwitterPersonality.count} twitter personalities."
-
-
-# Load customers 1009557540
-# csv_text = File.read('db/customers_cognitive_bank.csv')
-# csv      = CSV.parse(csv_text, :headers => true)
-# csv.first(220).each_with_index do |row, i|
-#   if i < TwitterPersonality.count-2
-#     cust                     = Customer.new(row.to_hash)
-#     cust.twitter_personality = TwitterPersonality.find(i+1)
-#     username                 = cust.twitter_personality.username || "user#{i}"
-#     cust.user                = User.new(name:     username,
-#                                         email:    username+'@example.com',
-#                                         password: 'password', password_confirmation: 'password')
-#     cust.save
-#     puts "#{i}:#{username}:#{cust.inspect}"
-#   elsif i == 19
-#     cust                     = Customer.new(row.to_hash)
-#     cust.twitter_personality = TwitterPersonality.find_by_username 'Jonathan_Soroff'
-#     username                 = cust.twitter_personality.username || "user#{i}"
-#     cust.user                = User.new(name:     username,
-#                                         email:    username+'@example.com',
-#                                         password: 'password', password_confirmation: 'password')
-#     cust.save
-#     puts "#{i}:#{username}:#{cust.inspect}"
-#   elsif i == 218
-#     cust                     = Customer.new(row.to_hash)
-#     cust.twitter_personality = TwitterPersonality.find_by_username 'gaelgreene'
-#     username                 = cust.twitter_personality.username || "user#{i}"
-#     cust.user                = User.new(name:     username,
-#                                         email:    username+'@example.com',
-#                                         password: 'password', password_confirmation: 'password')
-#     cust.save
-#     puts "#{i}:#{username}:#{cust.inspect}"
-#   end
-# end
 
 puts 'Loading the customers and transactions...'
 csv_text = File.read('db/bruce_profile.csv')
 CSV.parse(csv_text, :headers => true) do |row|
-  cust                     = Customer.new(row.to_hash)
-  cust.twitter_personality = TwitterPersonality.find(1)
-  username                 = cust.twitter_personality.username || "user#{i}"
-  cust.user                = User.new(name:     username,
-                                      email:    username+'@example.com',
-                                      password: 'password', password_confirmation: 'password')
+  cust      = Customer.new(row.to_hash.except('name'))
+  cust.user = User.new(name:     row['name'],
+                       email:    row['name'].downcase + '@example.com',
+                       password: 'password', password_confirmation: 'password')
   cust.save
-  puts "#{1}:#{username}:#{cust.inspect}"
+  puts "#{1}:#{cust.user.name}:#{cust.inspect}"
 end
-
 puts "Loaded #{Customer.count} customers."
+
+
+# Load the twitter personalities
+i = 1
+CSV.foreach('db/bruce_twitter.csv', headers: true) do |row|
+  tp   = TwitterPersonality.create(row.to_hash.except('category'))
+  cust = Customer.joins(:user).where(users: { name: tp.username }).first
+  cust.twitter_personality = tp
+  tp.save
+end
+puts "Loaded #{TwitterPersonality.count} twitter personalities."
+
 
 # # Load the transaction categories
 # CSV.foreach('db/transaction_categories.csv', headers: true) do |row|
@@ -74,14 +33,15 @@ puts "Loaded #{Customer.count} customers."
 # 
 # 
 
+
 customer = Customer.first
 CSV.foreach('db/bruce_transactions.csv', headers: true) do |row|
-  
+
   # add the transaction
   customer.transactions.build(customer_id: row['CUSTID'],
                               date:        Date.strptime(row['DATE'], '%m/%d/%Y'),
                               # amount:                  row['transaction_amount'],
-                              category:    row['CATEGORY'])
+                              category: row['CATEGORY'])
 end
 customer.save
 

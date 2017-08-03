49186960d9fa7d931ac3ca61bf495fd041f75467
David Thomason <dtom90@users.noreply.github.com>
2017-08-03 17:38:37 -0400

    Failsafe for NLU

--- app/lib/util.rb
+++ app/lib/util.rb
@@ -1,14 +1,18 @@
 
 # General Utilities
 class Util
-  def self.load_default_score
-    puts 'loading sample'
+  def self.handle_score_error
     file = File.read('db/churn_result.json')
     JSON.parse(file)
   end
 
-  def self.load_default_personality
+  def self.handle_wpi_error
     file = File.read('db/buce-personality.json')
+    JSON.parse(file)
+  end
+  
+  def self.handle_nlu_error
+    file = File.read('db/bruce_tweet_keywords.json')
     JSON.parse(file)
   end
 end

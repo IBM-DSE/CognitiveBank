bd850b7d7189ba7bdf2c95f390229d559f473002
David Thomason <dtom90@users.noreply.github.com>
2017-08-14 11:25:53 -0400

    Added .travis_config.sh to set APP_URL for specific TRAVIS_BRANCH

---
+++ .travis_config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo Travis Branch: $TRAVIS_BRANCH
+
+if [[ ${TRAVIS_BRANCH} == "deploy-w3ibm-prod-us-south-ATAT-CognitiveBank"* ]]; then
+    APP_URL="https://cognitivebank.w3ibm.mybluemix.net/"
+elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-ATAT-pub-CognitiveBank"* ]]; then
+    APP_URL="https://cognitivebank-demo.mybluemix.net/"
+elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-eu-gb-ATAT-pub-CognitiveBank"* ]]; then
+    APP_URL="https://cognitivebank.eu-gb.mybluemix.net/"
+elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-au-syd-ATAT-pub-CognitiveBank"* ]]; then
+    APP_URL="https://cognitivebank.au-syd.mybluemix.net/"
+else
+    APP_URL=local
+fi
+
+echo Testing Target: $APP_URL

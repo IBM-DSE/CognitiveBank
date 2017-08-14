09d4854ef7ff13cb1ed8cc8fc3b0a5d8f7d4c3c7
David Thomason <dlthomas@us.ibm.com>
2017-08-14 13:19:42 -0400

    Fix to export APP_URL, sourcing .travix_config, db:migrate in script

--- .travis_config.sh
+++ .travis_config.sh
@@ -2,16 +2,20 @@
 
 echo Travis Branch: $TRAVIS_BRANCH
 
-if [[ ${TRAVIS_BRANCH} == "deploy-w3ibm-prod-us-south-ATAT-CognitiveBank"* ]]; then
-    APP_URL="https://cognitivebank.w3ibm.mybluemix.net/"
-elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-ATAT-pub-CognitiveBank"* ]]; then
+if [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-ATAT-pub-CognitiveBank"* ]]; then
     APP_URL="https://cognitivebank-demo.mybluemix.net/"
 elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-eu-gb-ATAT-pub-CognitiveBank"* ]]; then
     APP_URL="https://cognitivebank.eu-gb.mybluemix.net/"
 elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-au-syd-ATAT-pub-CognitiveBank"* ]]; then
     APP_URL="https://cognitivebank.au-syd.mybluemix.net/"
-else
-    APP_URL=local
+#if [[ ${TRAVIS_BRANCH} == "deploy-w3ibm-prod-us-south-ATAT-CognitiveBank"* ]]; then
+#    APP_URL="https://cognitivebank.w3ibm.mybluemix.net/"
 fi
 
+if [ ! -z "$APP_URL" ]; then
+    export APP_URL
+else
+    APP_URL=local
+    bin/rails db:migrate RAILS_ENV=test
+fi
 echo Testing Target: $APP_URL

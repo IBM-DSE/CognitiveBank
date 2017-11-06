b24fad33dfc9c159888d8723327682f2a9ef32c0
David Thomason <dlthomas@us.ibm.com>
2017-11-06 09:58:49 -0500

    Cosmetic changes

--- .travis_config.sh
+++ .travis_config.sh
@@ -8,8 +8,6 @@ elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-eu-gb-ATAT-pub-CognitiveBank"* ]]; th
     APP_URL="https://cognitivebank.eu-gb.mybluemix.net"
 elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-au-syd-ATAT-pub-CognitiveBank"* ]]; then
     APP_URL="https://cognitivebank.au-syd.mybluemix.net"
-#if [[ ${TRAVIS_BRANCH} == "deploy-w3ibm-prod-us-south-ATAT-CognitiveBank"* ]]; then
-#    APP_URL="https://cognitivebank.w3ibm.mybluemix.net"
 fi
 
 if [ ! -z "$APP_URL" ]; then

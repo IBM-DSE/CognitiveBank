8969a0cc4698b5d28f3057054648fc7cf98dd7f3
David Thomason <dlthomas@us.ibm.com>
2017-11-07 13:55:24 -0500

    Added admin spec

---
+++ spec/features/admin_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+feature 'Admin' do
+
+  scenario 'Manage Admin Panel' do
+
+    # Visit home page and click 'Log in'
+    visit '/'
+    expect(page).to have_text 'Cognitive Bank'
+    expect(page).to have_text 'Log in'
+    click_link 'Log in'
+
+    # Fill in log in credentials
+    within('h1') { expect(page).to have_text 'Log in' }
+    within 'form' do
+      expect(page).to have_text 'Email'
+      expect(page).to have_text 'Password'
+      fill_in 'Email', with: 'david@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+    end
+
+    expect_admin_panel
+    
+    # Create new machine learning service
+    click_link 'New Machine Learning Service'
+    expect(page).to_not have_link 'New Machine Learning Service'
+    expect(page).to have_text 'New Machine Learning Service'
+    expect(page).to have_text 'Name: Hostname: Username: Password: Deployment:'
+    expect(page).to have_text 'MLZ Settings Ldap port: Scoring hostname: Scoring port:'
+    expect(page).to have_button 'Create Machine Learning Scoring Service'
+
+    # Fill in new Machine Learning Service
+    # fill_in 'Name:', with: 'test'
+    # fill_in 'Hostname:', with: 'ibm-watson-ml.mybluemix.net'
+    # fill_in 'Username:', with: 'test'
+    # fill_in 'Password:', with: 'test'
+    # fill_in 'Deployment:', with: 'test'
+    # click_button 'Create Machine Learning Scoring Service'
+    # expect_admin_panel
+  end
+  
+end
+
+def expect_admin_panel
+  expect(page).to have_text 'Admin Panel'
+  expect(page).to have_text 'Welcome, David Thomason!'
+  expect(page).to have_text 'You are logged in as administrator'
+
+  expect(page).to have_text 'Customers:'
+  expect(page).to have_text 'Name Churn Prediction: Churn Probability: Last Scoring Call: Profile'
+  expect(page).to have_text 'Bruce View Profile'
+
+  expect(page).to have_text 'Machine Learning Services:'
+  expect(page).to have_text 'Id Name Hostname Deployment Authentication ML Scoring Actions'
+  expect(page).to have_link 'New Machine Learning Service'
+end

72e5506635f80ab01fe064a8f54b67fd1909846d
David Thomason <dlthomas@us.ibm.com>
2017-11-15 19:36:31 -0500

    Currency nbsp

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,44 +1,43 @@
 module CustomersHelper
   
-  MAX=100
+  MAX = 100
   
-  MIN_AIRFARE=1000
-  MAX_AIRFARE=5000
+  MIN_AIRFARE = 1000
+  MAX_AIRFARE = 5000
   
-  MIN_HOTEL=300
-  MAX_HOTEL=1000
+  MIN_HOTEL = 300
+  MAX_HOTEL = 1000
   
-  MIN_DINING=80
-  MAX_DINING=150
+  MIN_DINING = 80
+  MAX_DINING = 150
   
-  MIN_SAFARI=800
-  MAX_SAFARI=1200
+  MIN_SAFARI = 800
+  MAX_SAFARI = 1200
   
   def rand_dollar(category, locale)
     
     dollars = case category
-                when 'Airfare'
-                  MIN_AIRFARE + rand * (MAX_AIRFARE-MIN_AIRFARE)
-                when 'Hotel'
-                  MIN_HOTEL + rand * (MAX_HOTEL-MIN_HOTEL)
-                when 'Dining'
-                  MIN_DINING + rand * (MAX_DINING-MIN_DINING)
-                when 'Safari'
-                  MIN_SAFARI + rand * (MAX_SAFARI-MIN_SAFARI)
-                else
-                  rand * MAX
+              when 'Airfare'
+                MIN_AIRFARE + rand * (MAX_AIRFARE - MIN_AIRFARE)
+              when 'Hotel'
+                MIN_HOTEL + rand * (MAX_HOTEL - MIN_HOTEL)
+              when 'Dining'
+                MIN_DINING + rand * (MAX_DINING - MIN_DINING)
+              when 'Safari'
+                MIN_SAFARI + rand * (MAX_SAFARI - MIN_SAFARI)
+              else
+                rand * MAX
               end
     
     num_to_currency dollars.round(2), locale: locale
   end
   
   def num_to_currency(number, options)
-    amount = number_with_precision(number, precision: 2)
     if options[:locale] == 'en-IN'
-      '₹ ' + number_with_delimiter(amount, delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/)
-    else
-      number_to_currency amount, options
+      number = number_with_precision(number, precision: 2)
+      number = number_with_delimiter(number, delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/)
     end
+    number_to_currency number, options
   end
   
   def rg_churn_cell(churn)
@@ -55,13 +54,11 @@ module CustomersHelper
       content_tag :div, class: 'flex' do
         content_tag(:div,
                     ('&nbsp;' + (churn ? percent : '')).html_safe,
-                    class: 'red-cell', style: "flex: #{churn_probability}"
-        )+
+                    class: 'red-cell', style: "flex: #{churn_probability}") +
           content_tag(:div,
                       ('&nbsp;' + (churn ? '' : percent)).html_safe,
                       class: 'green-cell', style: "flex: #{1 - churn_probability}")
       end
     end
   end
-
 end

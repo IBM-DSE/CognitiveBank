3fabfcbc8080e26747949781b2206a7140caacd0
David Thomason <dtom90@users.noreply.github.com>
2017-11-17 11:31:48 -0500

    Only testing against public Bluemix

--- .travis_config.sh
+++ .travis_config.sh
@@ -2,14 +2,8 @@
 
 echo Travis Branch: $TRAVIS_BRANCH
 
-if [[ ${TRAVIS_BRANCH} == "deploy-w3ibm-prod-us-south-ATAT-CognitiveBank"* ]]; then
-    APP_URL="https://cognitivebank-test.w3ibm.mybluemix.net/"
-elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-ATAT-pub-CognitiveBank"* ]]; then
-    APP_URL="https://cognitivebank-demo.mybluemix.net"
-elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-eu-gb-ATAT-pub-CognitiveBank"* ]]; then
-    APP_URL="https://cognitivebank.eu-gb.mybluemix.net"
-elif [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-au-syd-ATAT-pub-CognitiveBank"* ]]; then
-    APP_URL="https://cognitivebank.au-syd.mybluemix.net"
+if [[ ${TRAVIS_BRANCH} == "deploy-ibm-yp-us-south-IBM-ATAT-CognitiveBank"* ]]; then
+    APP_URL="https://cognitivebank-test.mybluemix.net/"
 fi
 
 if [ ! -z "$APP_URL" ]; then

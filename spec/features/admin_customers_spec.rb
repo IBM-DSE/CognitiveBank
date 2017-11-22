ef14dcd76804810f5c42ad260cd36790beab77c0
David Thomason <dlthomas@us.ibm.com>
2017-11-22 15:34:05 -0500

    Added feature spec for custom image upload

--- spec/features/admin_customers_spec.rb
+++ spec/features/admin_customers_spec.rb
@@ -16,13 +16,12 @@ feature 'Administrate Customers', include_shared: true do
   
   end
   
-  scenario 'Create Sally' do
+  scenario 'Create Two Sallys' do
     
     # Create first Sally
     create_sally
     
     # Expect her profile
-    expect(page).to have_text "Sally's Profile:"
     expect(page).to have_text 'sally@example.com'
     expect_sally_profile
     
@@ -33,8 +32,7 @@ feature 'Administrate Customers', include_shared: true do
     # Create second Sally
     create_sally
     
-    # Expect her profile
-    expect(page).to have_text "Sally's Profile:"
+    # Expect her profile with email sally1@example.com
     expect(page).to have_text 'sally1@example.com'
     expect_sally_profile
     
@@ -58,12 +56,58 @@ feature 'Administrate Customers', include_shared: true do
     expect(page).to have_text 'Last Statement Balance:'
   
   end
+  
+  scenario 'Create David With Custom Image' do
+    
+    click_link 'New Customer'
+    expect_customer_form
+    
+    # Fill in required fields
+    fill_in 'Name', with: 'David'
+    fill_in 'Twitter Handle', with: 'dtom90'
+    fill_in 'Age', with: '27'
+    fill_in 'Activity', with: '3'
+    select "Master's Degree", from: 'Education Level'
+    select 'Male', from: 'Gender'
+    fill_in 'State', with: 'NY'
+    fill_in 'Negative Tweets', with: '0'
+    fill_in 'Income', with: '2000'
+    attach_file 'Custom Image:', Rails.root + 'spec/features/David.jpeg'
+    
+    click_button 'Create Customer'
+    
+    expect(page).to have_text "David's Profile:"
+    expect(page).to have_css("img[src*='David.jpeg']")
+    expect(page).to have_text 'david@example.com'
+    
+    expect(page).to have_text 'Gender: Male'
+    expect(page).to have_text 'Age: 27'
+    expect(page).to have_text 'State: NY'
+    expect(page).to have_text 'Education Level: Masters degree'
+    expect(page).to have_text '$ 2,000.00'
+    expect(page).to have_text 'Investment:'
+    expect(page).to have_text 'Annual Spending:'
+    expect(page).to have_text 'Annual Transactions:'
+    expect(page).to have_text 'Average Daily Transactions:'
+    expect(page).to have_text 'Average Transaction Amount:'
+    
+    expect(page).to have_text 'Twitter Profile:'
+    expect(page).to have_text '@dtom90'
+    expect(page).to have_text 'Negative Finance-Related Tweets: 0'
+    expect(page).to have_text 'Keywords'
+    expect(page).to have_text 'Sentiment'
+    expect(page).to have_text 'foreign exchange fees 0%'
+    
+    expect(page).to have_text 'Personality Insights:'
+    expect(page).to have_text 'Needs:	Practicality'
+    expect(page).to have_text 'Values: Self-transcendence'
+  
+  end
 
 end
 
-def create_sally
-  
-  click_link 'New Customer'
+
+def expect_customer_form
   
   # Required fields
   expect(page).to have_text 'Name*: Twitter Handle*: Age*: Activity*:'
@@ -77,6 +121,14 @@ def create_sally
   expect(page).to have_text 'Investment: Annual Spending: Annual Transactions:'
   expect(page).to have_text 'Average Daily Transactions: Average Transaction Amount:'
   expect(page).to have_select 'Nationality:', with_options: ['United States ($)', 'India (₹)']
+  expect(page).to have_field 'Custom Image:'
+
+end
+
+def create_sally
+  
+  click_link 'New Customer'
+  expect_customer_form
   
   # Fill in required fields
   fill_in 'Name', with: 'Sally'
@@ -95,6 +147,7 @@ end
 
 def expect_sally_profile
   
+  expect(page).to have_text "Sally's Profile:"
   expect(page).to have_text 'Gender: Female'
   expect(page).to have_text 'Age: 22'
   expect(page).to have_text 'State: FL'
